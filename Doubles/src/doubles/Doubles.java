d0200545768966264740bea88c13a709c258dee9
unknowntaken/ <ngokethan@gmail.com>
2017-07-24 16:19:26 -0700

    Doubles

---
+++ Doubles/src/doubles/Doubles.java
@@ -0,0 +1,21 @@
+
+package doubles;
+
+import java.util.Scanner;
+
+public class Doubles {
+
+   
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int random = (int)(Math.random() * 6 + 1);
+        System.out.println(random);
+        int random2 = (int)(Math.random() * 6 + 1);
+        System.out.println(random2);
+        
+        while (random!=random2) {
+            
+        }
+    }
+    
+}
