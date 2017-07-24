3abb68d444d6186f70044761eb5533058d54a437
unknowntaken/ <ngokethan@gmail.com>
2017-07-24 11:31:23 -0700

    Person2

---
+++ Person2/src/person2/Person2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person2;
+
+/**
+ *
+ * @author Education Unlimited
+ */
+public class Person2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int age = 14;
+        double height = 5.3;
+        String firstName = " Gabe";
+        String lastName = " Tejeda";
+        boolean isAStudent = true;
+        boolean isAHuman = false;
+        double IQ = 0.0;
+        System.out.println("My name is"+firstName+" "+lastName);
+        System.out.println("I am a human "+isAHuman+" and my IQ is "+IQ);
+    }
+    
+}
