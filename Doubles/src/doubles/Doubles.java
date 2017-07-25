c1130d457300a0983f4b319129e7bfa42a331a08
unknowntaken/ <ngokethan@gmail.com>
2017-07-24 19:04:23 -0700

    Doubles

--- Doubles/src/doubles/Doubles.java
+++ Doubles/src/doubles/Doubles.java
@@ -14,8 +14,17 @@ public class Doubles {
         System.out.println(random2);
         
         while (random!=random2) {
-            
+            System.out.println("Sorry, you didn't get a double.");
+            System.out.println("Type anything to continue.");
+            String yes = keyboard.nextLine();
+            random = (int)(Math.random() * 6 + 1);
+            System.out.println(random);
+            random2 = (int)(Math.random() * 6 + 1);
+            System.out.println(random2);
         }
+            System.out.println("Congratulations! You rolled a double.");
+
+
     }
     
 }
