f7153f2df060ae290ef0100b0339424d2e82431d
unknowntaken/ <ngokethan@gmail.com>
2017-07-27 12:21:02 -0700

    Challenge Adventure

---
+++ ChooseYourOwnAdventure/src/chooseyourownadventure/ChooseYourOwnAdventure.java
@@ -0,0 +1,149 @@
+package chooseyourownadventure;
+
+import java.util.Scanner;
+
+public class ChooseYourOwnAdventure {
+
+    public static void main(String[] args) {
+
+        try {
+            Scanner keyboard = new Scanner(System.in);
+
+            System.out.println("You've just left a party but have no idea where you are.");
+            Thread.sleep(2500);
+            System.out.println("You can't go back because you've thoroughly embarassed yourself and you lost your phone, so you decide to take a walk instead of just waiting for your ride.");
+            Thread.sleep(5000);
+            System.out.println("You've been walking down a street with woods on either side of you, but you still have at least another hour to wait.");
+            Thread.sleep(4500);
+            System.out.println("You keep walking.");
+            Thread.sleep(2500);
+            System.out.println("You're starting to think it's time to head back, when you begin hearing IT.");
+            Thread.sleep(3500);
+            System.out.println("Twigs, branches, or leaves - you can't tell which - are cracking behind you.");
+            Thread.sleep(3500);
+            System.out.println("You tell yourself it's just a squirrel. But later, you'll look back and wish it were so small.");
+            Thread.sleep(4000);
+            System.out.println("\tA) Do you keep walking away from it?");
+            System.out.println("\tB) Or do you go back to see what it is?");
+            String answer1 = keyboard.nextLine();
+            if (answer1.equals("A")) {
+                System.out.println("You've decided to keep walking, but you start to speed up when you hear it's growl. The growl of a predator.");
+                Thread.sleep(4500);
+                System.out.println("It doesn't go away and you're too afraid to face it, so you keep speeding up until you're at a run. But you can't go fast enough.");
+                Thread.sleep(5500);
+                System.out.println("You're heart is beating faster than you thought possible from the exercise, adrenaline, and fear.");
+                Thread.sleep(4500);
+                System.out.println("For just a second you don't pay attention to the ground and trip.");
+                Thread.sleep(3500);
+                System.out.println("For the first time you look back, and you finally catch a glimpse of it. It's so dark, all you can see are it's eyes. ");
+                Thread.sleep(5000);
+                System.out.println("They must belong to a dragon! Huge, yellow, snake-like, glowing like nothing you've ever seen.");
+                Thread.sleep(3500);
+                System.out.println("It's too late. You have to run into the forest for protection. You won't stand a chance out on the street.");
+                Thread.sleep(3500);
+                System.out.println("Sprinting, as fast as you can, but you're not gaining any ground. You hear it behind you, closing in on you: the prey, and it: your predator.");
+                Thread.sleep(5000);
+                System.out.println("You can barely see anything, and again, you trip but this time you fall.");
+                Thread.sleep(3000);
+                System.out.println("Down a long, steep hill, you keep rolling faster.");
+                Thread.sleep(3500);
+                System.out.println("When you land, you can still hear it, but fainter. All your bones ache from the fall. You feel weak.");
+                Thread.sleep(5000);
+                System.out.println("Your only salvation, a house! It's dark, but it must be.");
+                Thread.sleep(3500);
+                System.out.println("Every fiber in your body is telling you not to go, but you must be so lucky to find a lonely house that you can hide in.");
+                Thread.sleep(5000);
+                System.out.println("No matter what is in there, it must be better than whatever monster is following you.");
+                Thread.sleep(5500);
+                System.out.println("You go inside, and realize you must be in a castle. A huge hall with what must have been at one point grand stairs.");
+                Thread.sleep(4000);
+                System.out.println("You need to go deeper into the castle, where you will be more protected.");
+                Thread.sleep(3000);
+                System.out.println("\tA) Do you want to go up the stairs where you might be more protected?");
+                System.out.println("\tB) Or into one of the many rooms and hallways that could have weapons?");
+                String answer2 = keyboard.nextLine();
+                if (answer2.equals("B")) {
+                    System.out.println("Weapons of course! What other chance do you have if it comes!");
+                    Thread.sleep(2500);
+                    System.out.println("You go through a set of doors that are twelve feet tall into a hallway.");
+                    Thread.sleep(3500);
+                    System.out.println("Maybe you'll find a suit of armor along the walls.");
+                    Thread.sleep(3500);
+                    System.out.println("By the time you're halfway down the hall, the massive doors begin to close.");
+                    Thread.sleep(4000);
+                    System.out.println("You run to catch them but it's too late. You can't budge them, forget opening them. You're stuck.");
+                    Thread.sleep(4500);
+                    System.out.println("The stress is killing you. Why can't anything be lit up. You can only see ten feet ahead of you due to the darkness.");
+                    Thread.sleep(4500);
+                    System.out.println("You're desperate.");
+                    Thread.sleep(2500);
+                    System.out.println("You keep walking but don't take your chances inside the rooms. The hallway just seems to keep going.");
+                    Thread.sleep(4500);
+                    System.out.println("Finally! Just like you hoped. One of those old suits of armor with a shield and sword.");
+                    Thread.sleep(4000);
+                    System.out.println("And you can lift the chestpiece of the armor to get in after undoing some metal latches.");
+                    Thread.sleep(3500);
+                    System.out.println("The only problem, is when you stick your arm in, is you can barely move your arms. The sword and shield would be almost useless.");
+                    Thread.sleep(3000);
+                    System.out.println("\tA) Will you climb into the armor and forfeit the sword and shield in the hopes that the armor will hold?");
+                    System.out.println("\tB) Or will you grab the sword and shield to be able to better handle your own safety but not be able to wear the armor?");
+                    String answer3 = keyboard.nextLine();
+                    if (answer3.equals("A")) {
+                        System.out.println("You don't even think you could use the sword that well, so you climb into the armor.");
+                        Thread.sleep(3500);
+                        System.out.println("It's a slow process, and it feels like the armor is shaking, but that must just be you.");
+                        Thread.sleep(3500);
+                        System.out.println("Once you're in, there's something that is holding you back from getting off the tiny pedestal.");
+                        Thread.sleep(3500);
+                        System.out.println("You hear it's a chain that must be attached to the back. You must not have noticed it before.");
+                        Thread.sleep(4500);
+                        System.out.println("You keep trying to force your way out, to break the chain, but then you can feel the shaking again.");
+                        Thread.sleep(3500);
+                        System.out.println("You finally stop a moment to get your bearings, and you realize the whole wall behind you is moving, turning.");
+                        Thread.sleep(3500);
+                        System.out.println("It's been moving this whole time, it's about to re-seal itself and put you on the other side of the wall.");
+                        Thread.sleep(3500);
+                        System.out.println("You stick out your arm in between the rest of the wall and the part of the wall that's about to close you off.");
+                        Thread.sleep(4500);
+                        System.out.println("The wall doesn't stop and you hear the crunching of metal. The left arm of your armor is being torn off and the shield is going with it.");
+                        Thread.sleep(4500);
+                        System.out.println("Your own arm is safe, but only for now. You've lost the armor from the elbow down. And you're a sitting duck.");
+                        Thread.sleep(4000);
+                        System.out.println("There's nothing you can do. You've had no control since the beginning!");
+                        Thread.sleep(3500);
+                        System.out.println("And that's when it occurs to you. Up to this point, nothing you have done has really been under your own power.");
+                        Thread.sleep(4000);
+                        System.out.println("Falling down a hill, doors closing, walls moving. Just like your fate was decided for you.");
+                        Thread.sleep(3500);
+                        System.out.println("That must be it, you've been stuck in a giant trap.");
+                        Thread.sleep(3000);
+                        System.out.println("The fear strikes you like a dagger in the pit of your heart. You look up, and there it is again!");
+                        Thread.sleep(4000);
+                        System.out.println("Those eyes! Dear Lord, it's hideous! You're face to face with the monster!");
+                        Thread.sleep(3500);
+                        System.out.println("Too scared to move, you know this is the end. You see intelligence in its eyes. And its next move.");
+                        Thread.sleep(3500);
+                        System.out.println("It starts with your weakness. Your left arm. It moves so fast you can't see it massive claws as they rip off your entire arm.");
+                        Thread.sleep(4000);
+                        System.out.println("The pain is so terrible, but the monster doesn't care. It breaks into the armor through your arm and never stops.");
+                        Thread.sleep(4000);
+                        System.out.println("The black hits you, and you can't feel anything. Everything is over.");
+                        Thread.sleep(3000);
+                        System.out.println("Caught in the elaborate trap of an ancient monster that has made you nothing but its next meal.");
+                        Thread.sleep(4000);
+                        System.out.println("GAME OVER: Eaten alive by the monster.");
+                    } else {
+
+                    }
+                } else {
+
+                }
+            } else {
+
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
